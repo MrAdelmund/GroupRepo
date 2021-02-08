07d05f3f5a2d9c7e843b7eedd2d2f347365d0434
Alejandra Gomez <21.alejandra.gomez@ksd.org>
2021-02-08 09:37:36 -0800

    adding my Brainstrom method to the hello script

--- GroupTestProject/Assets/hello.cs
+++ GroupTestProject/Assets/hello.cs
@@ -8,11 +8,17 @@ public class hello : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello there!");
+        Brainstrom();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Brainstrom()
+    {
+        Debug.Log("Hi I guess");
     }
 }
